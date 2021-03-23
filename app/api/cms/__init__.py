1c7a259c9da4fd09ab2604da18b3b5f7a55594ee
renserve <1249485588@qq.com>
2021-03-23 17:07:50 +0800

    bigfile

--- app/api/cms/__init__.py
+++ app/api/cms/__init__.py
@@ -14,12 +14,14 @@ def create_cms():
     from .user import user_api
     from .log import log_api
     from .file import file_api
+    from .bigfile import bigfile_api
     from .test import test_api
     from .file_group import file_group_api
     admin_api.register(cms)
     user_api.register(cms)
     log_api.register(cms)
     file_api.register(cms)
+    bigfile_api.register(cms)
     test_api.register(cms)
     file_group_api.register(cms)
     return cms
